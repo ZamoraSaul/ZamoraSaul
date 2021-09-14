f9e8d93f634a856da7b7da9085baa531365030bd
Saul <zamorasaul275@gmail.com>
2021-09-13 20:30:08 -0600

    commit de cambio

--- js/main.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("iconoMenu").addEventListener("click", mostrarMenu);
-function mostrarMenu() {
-    document.getElementById("mostrarMenu").classList.toggle('mostrarLateral');
-}
-document.getElementById("botonCerrar").addEventListener("click", cerrarMenu);
-function cerrarMenu(){
-    document.getElementById("mostrarMenu").classList.toggle('mostrarLateral');
-}
